2f7ae4f789a1c1ebde125333c273b21fa2977673
Patrick Hughlett <phughlett87@gmail.com>
2022-03-22 22:36:46 -0600

    code cleanup

--- src/App.js
+++ src/App.js
@@ -87,28 +87,6 @@ function App() {
 
   }
 
-  function PlaceholderPage() {
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-
-    )
-
-  }
 
   function NoMatch() {
 
